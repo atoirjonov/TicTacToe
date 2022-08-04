621dc20d442d5c7367ad4421dd2249f72d5bf0a3
Akhmadsher Toirjonov <akhmadshertoirjonov@gmail.com>
2022-08-04 10:55:41 +0400

    Initial commit. Create 2D array for empty chars.

---
+++ src/TicTacToe.java
@@ -0,0 +1,13 @@
+public class TicTacToe {
+    public static void main(String[] args) {
+        /*
+        Create a 2D array named board in order
+        to create 3 x 3 board
+         */
+        char [][] board = {
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+        };
+    }
+}
