1c00cd98f97f9056f78fd8e2cc4bafb811a62ec2
Akhmadsher Toirjonov <akhmadshertoirjonov@gmail.com>
2022-08-04 11:27:38 +0400

    Create playerTurn(board, userInput) method.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -13,12 +13,49 @@ public class TicTacToe {
                 {' ', ' ', ' '}
         };
 
-        printBoard(board);
+
 
         // Takes the user input
         System.out.println("What cell would you like to chose?");
         String userInput = scanner.nextLine();
-        System.out.println(userInput);
+
+
+        playerTurn(board, userInput);
+        printBoard(board);
+    }
+
+    private static void playerTurn(char[][] board, String userInput) {
+        switch(userInput) {
+            case "1":
+                board[0][0] = 'X';
+                break;
+            case "2":
+                board[0][1] = 'X';
+                break;
+            case "3":
+                board[0][2] = 'X';
+                break;
+            case "4":
+                board[1][0] = 'X';
+                break;
+            case "5":
+                board[1][1] = 'X';
+                break;
+            case "6":
+                board[1][2] = 'X';
+                break;
+            case "7":
+                board[2][0] = 'X';
+                break;
+            case "8":
+                board[2][1] = 'X';
+                break;
+            case "9":
+                board[2][2] = 'X';
+                break;
+            default:
+                System.out.println("Error!");
+        }
     }
 
     private static void printBoard(char[][] board) {
