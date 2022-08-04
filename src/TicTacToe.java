71827ec65a57b13d30a5897db64e5776afbd8ce0
Akhmadsher Toirjonov <akhmadshertoirjonov@gmail.com>
2022-08-04 11:04:50 +0400

    Print out the first line of the board.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -9,5 +9,7 @@ public class TicTacToe {
                 {' ', ' ', ' '},
                 {' ', ' ', ' '}
         };
+
+        System.out.println(board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
     }
 }
