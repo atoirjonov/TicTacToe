3185181d17cb8dcfaa197f6992b55051cde278e2
Akhmadsher Toirjonov <akhmadshertoirjonov@gmail.com>
2022-08-04 11:07:07 +0400

    Print out the second and third lines of the board. Add 'X' inside the empty characters to check if the board array displays as expected.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -5,11 +5,13 @@ public class TicTacToe {
         to create 3 x 3 board
          */
         char [][] board = {
-                {' ', ' ', ' '},
-                {' ', ' ', ' '},
-                {' ', ' ', ' '}
+                {'X', 'X', 'X'},
+                {'X', 'X', 'X'},
+                {'X', 'X', 'X'}
         };
 
         System.out.println(board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
+        System.out.println(board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
+        System.out.println(board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
     }
 }
