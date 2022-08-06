76e2fa84d6d3350fb9e54db386e4e49252270df9
Akhmadsher Toirjonov <akhmadshertoirjonov@gmail.com>
2022-08-06 11:38:36 +0400

    Create isGameFinished(board) method. For now it just return true. Modify the while loop by adding check isFinished() after each player move.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random rand = new Random();
+     Scanner scanner = new Scanner(System.in);
+
+
         /*
-        Create a 2D array named board in order
+        Creates a 2D array named board in order
         to create 3 x 3 board
          */
         char [][] board = {
@@ -15,63 +16,123 @@ public class TicTacToe {
                 {' ', ' ', ' '}
         };
 
-        playerMove(board, scanner);
         printBoard(board);
-        while (true) {
 
-            // Get a random number between and 1 and 9
-            int computerMove = rand.nextInt(9) + 1;
-            if (isValidMove(board, computerMove)) {
+        // The while that decides whether the game continues until the board is full.
+        while (true) {
+            // User's turn
+            playerTurn(board, scanner);
+            if(isGameFinished(board)) {
                 break;
             }
+            printBoard(board);
+
+            // Computer's turn
+            computerTurn(board);
+            if(isGameFinished(board)) {
+                break;
+            }
+            printBoard(board);
         }
+
 
     }
 
+    private static boolean isGameOver(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
+    /**
+     * Computer generates random number
+     *
+     * @param board
+     */
+    private static void computerTurn(char[][] board) {
+        Random rand = new Random();
+        int computerMove;
+        while(true) {
+            computerMove = rand.nextInt(9) + 1;
+            if(isValidMove(board, computerMove)) {
+                break;
+            }
+        }
+        System.out.println("Computer chose " + computerMove);
+        placeMove(board, Integer.toString(computerMove), 'O');
+    }
 
+    /**
+     * Lets the player chose their move by entering a number
+     * between 1 and 9.
+     *
+     * @param board
+     * @param scanner
+     */
+    private static void playerTurn(char[][] board, Scanner scanner) {
+        String userInput;
+        while (true) {
+            System.out.println("What cell would you like to chose(1-9)?");
+            userInput = scanner.nextLine();
+            if(isValidMove(board, Integer.parseInt(userInput))) {
+                break;
+            } else {
+                System.out.println("You have made a wrong move.");
+            }
+        }
+        placeMove(board, userInput, 'X');
+    }
 
-
-
-
-
-    private static void playerMove(char[][] board, Scanner scanner) {
-        System.out.println("What cell would you like to chose(1-9) ?");
-        String userInput = scanner.nextLine();
-
-        switch(userInput) {
+    /**
+     * Inserts or places a move of a player.
+     *
+     * @param board
+     * @param position
+     * @param symbol
+     */
+    private static void placeMove(char[][] board, String position, char symbol) {
+        switch(position) {
             case "1":
-                board[0][0] = 'X';
+                board[0][0] = symbol;
                 break;
             case "2":
-                board[0][1] = 'X';
+                board[0][1] = symbol;
                 break;
             case "3":
-                board[0][2] = 'X';
+                board[0][2] = symbol;
                 break;
             case "4":
-                board[1][0] = 'X';
+                board[1][0] = symbol;
                 break;
             case "5":
-                board[1][1] = 'X';
+                board[1][1] = symbol;
                 break;
             case "6":
-                board[1][2] = 'X';
+                board[1][2] = symbol;
                 break;
             case "7":
-                board[2][0] = 'X';
+                board[2][0] = symbol;
                 break;
             case "8":
-                board[2][1] = 'X';
+                board[2][1] = symbol;
                 break;
             case "9":
-                board[2][2] = 'X';
+                board[2][2] = symbol;
                 break;
             default:
                 System.out.println("Error!");
         }
     }
 
+    /**
+     * Prints out an empty 3x3 board in the console.
+     * @param board
+     */
     private static void printBoard(char[][] board) {
         System.out.println(board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
         System.out.println(board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
@@ -79,7 +140,10 @@ public class TicTacToe {
     }
 
     /**
-    Checks whether the position is available
+     * Checks whether the position is available
+     * @param board
+     * @param position
+     * @return
      */
     private static boolean isValidMove(char[][] board, int position) {
         switch (position) {
@@ -106,4 +170,15 @@ public class TicTacToe {
         }
 
     }
+
+
+    /**
+     * Breaks the game is the board is full.
+     * @param baord
+     */
+    private static boolean isGameFinished(char[][] baord) {
+        return true;
+    }
+
+
 }
